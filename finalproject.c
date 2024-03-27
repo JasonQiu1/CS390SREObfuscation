983266ade436be70e10649818aab2bd8358589f3
Jason Qiu <jason.blank.qiu@gmail.com>
2024-03-27 18:20:12 -0400

    feat: initial commit with starter code and some bloated assembly

---
+++ finalproject.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+#include "string.h"
+
+int transform(char* digits) {
+	int res = 0;
+	for (int i = 0; i < strlen(digits); i++) {
+		res += digits[i];
+	}
+	return res;
+}
+
+int garbage() {
+	int i = 0;
+	i += 5;
+	i /= 2;
+	i *= 15;
+	return i;
+}
+
+int main(int argc, char** argv) {
+	if (argc == 1) {
+		printf("The first number is %d.\n", transform("red herring?"));
+		printf("The second number is %d.\n", garbage());
+		if (transform("which branch?") < 1000) {
+			printf("AWOOGA!");
+		} else {
+			puts("HMMMMMM");
+		}
+		return 0;
+	} else if (strcmp(argv[1], "secret") == 0) {
+		puts("you found the secret");
+	}
+	return 0;
+}
