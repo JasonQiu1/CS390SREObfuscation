ab3211857764eb7281d0cf9fa718472720a6fa5e
aloyeoshotse <aloye.oshotse@gmail.com>
2024-04-06 10:04:56 -0400

    made chages to c code

--- finalproject.c
+++ finalproject.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include <stdlib.h>
 #include "string.h"
 
 int transform(char* digits) {
@@ -20,7 +21,25 @@ int garbage() {
 	return i;
 }
 
+char *cocomelon() {
+    char *str = (char*)malloc(7 * sizeof(char)); 
+    if (str == NULL) {
+        exit(1); 
+    }
+    strcpy(str, "AwULGe"); 
+    str[0] = 's';
+    str[1] = 'e';
+    str[2] = 'c';
+    str[3] = 'r';
+    str[4] = 'e';
+    str[5] = 't';
+    return str;
+}
+
 int main(int argc, char** argv) {
+
+	char *var1 = cocomelon();
+
 	if (argc == 1) {
 		printf("The first number is %d.\n", transform("red herring?"));
 		printf("The second number is %d.\n", garbage());
@@ -30,10 +49,11 @@ int main(int argc, char** argv) {
 			puts("HMMMMMM");
 		}
 		return 0;
-	} else if (strcmp(argv[1], "secret") == 0) {
+	} else if (strcmp(argv[1], var1) == 0) {
 		puts("Correct password entered, the location is: ...");
 	}else{
 		puts("you didn't enter the correct password");
 	}
+	free(var1);
 	return 0;
 }
